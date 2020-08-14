e42d7419c6897d11fa2fcd6ac91fe7c2d2d17ea0
Burak Yoruk <ibyoruk@gmail.com>
2020-08-14 08:09:03 -0400

    Add project files.

---
+++ BgpProjectExtender/Functions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DriveWorks.Extensibility;
+using Titan.Rules.Common;
+using Titan.Rules.Execution;
+
+namespace BlueGiant
+{
+    public class Functions : ProjectExtender
+    {
+        [Udf(true)]
+        [FunctionInfo("A universal function to populate all file name, configuration, and user assigned custom properties for all captured master parts, drawings, and assemblies. Requires calculation table with master file names in first column, and master property names in header row - as given by the MyName() function.", "SDK Function")]
+        public object PopulateMasterFileProperty([ParamInfo("CalcTable", "The calculation table with the file property rules.")] IArrayValue calcTable, [ParamInfo("MyName() return", "Always use myName() function here.")] string myName)
+        {
+            if (myName.StartsWith("DWVariable")) return "Cannot apply function inside variable rules, use function directly within Model Rules";
+
+            string[] myNameSplitList = myName.Split('\\');
+
+            string masterFileName = myNameSplitList[myNameSplitList.Length - 2];
+            string filePropertyName = myNameSplitList[myNameSplitList.Length - 1];
+
+            int columnIndex = (int)TableFunctions.TableGetColumnIndexByName(calcTable, filePropertyName, true) - 1;
+
+            string firstColumnPipebarList = (string)TableFunctions.ListAll(calcTable, 1);
+            string[] firstColumnSplitList = firstColumnPipebarList.Split('|');
+            int rowIndex = Array.IndexOf(firstColumnSplitList, masterFileName) + 1;
+
+            return calcTable.GetElement(rowIndex, columnIndex);
+        }
+    }
+}
---
+++ BgpProjectExtender/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BgpProjectExtender")]
+[assembly: AssemblyDescription("Provides Extended Functions for use in DriveWorks")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BgpProjectExtender")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(true)]
+
+[assembly: Guid("4fc24480-6b5d-4b3a-8d0d-893803126707")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
